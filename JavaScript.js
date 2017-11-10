779ec5e1fa3af00016753f415a5cba6560ce6942
eacallam <eacallam@yahoo.com>
2017-11-09 20:42:45 -0500

    First Commit

---
+++ JavaScript.js
@@ -0,0 +1,25 @@
+﻿var myDiv = document.getElementById("something");
+var myButton = document.getElementById("btn");
+var result = printFizzBuzz();
+
+myButton.onclick = function () { myDiv.innerHTML = result };
+
+function printFizzBuzz() {
+    var returnVal = ""; //initializing the variable to an empty string
+    for (var i = 1; i <= 100; i++) {
+        if (i % 15 === 0) {
+            returnVal += "FIZZBUZZ";
+        }
+        else if (i % 3 === 0) {
+            returnVal += "FIZZ";
+        }
+        else if (i % 5 === 0) {
+            returnVal += "BUZZ";
+        }
+        else {
+            returnVal += i;
+        }
+        returnVal += "<br/>"
+    }
+    return returnVal;
+}
